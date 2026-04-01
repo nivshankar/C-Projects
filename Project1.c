413b266d22d381f92365b8927025aafde284e18f
nivshankar <nivshankar9@gmail.com>
2026-04-01 21:42:19 +0530

    Add files via upload

---
+++ Project1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float a;
+	printf("Enter TEmperature in Celsius: \n");
+	scanf("%f",&a);
+	printf("The temperature in farenheit is: %.2f",((9.0/5.0)*a+32));
+}
+/*
+Output:
+Enter TEmperature in Celsius:
+67
+The temperature in farenheit is: 152.60
+*/
